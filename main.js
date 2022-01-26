382b5e45b2be0c0fe8a5709887d7ec6b32868c4d
katy millard <katy.millard@gmail.com>
2022-01-26 11:39:48 -0600

    Third commit

--- main.js
+++ main.js
@@ -4,20 +4,19 @@ async function main() {
         const drivingAge = 18;
         const myAge = 15;
         const hasLicense = await input.confirm('Do you have your license?');
-        const n = (18 - myAge);
+        const n = (drivingAge - myAge);
 
     if (myAge >= drivingAge && hasLicense) {
         console.log('You can drive!');
-    }
-
-    else {
-        if (myAge < drivingAge){
-            console.log('Wait ' + n + ' years!');
+    }   else {
+            if (myAge < drivingAge){
+                console.log('Wait ' + n + ' years!');
+            }
+            else{
+                console.log('Go get your license!')
+            }
         }
-        else{
-            console.log('Go get your license!')
-        }
     }
-}
-
-main();
+    
+    main();
+    
