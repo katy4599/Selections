30a26ceaa21dbefe00c3f5f7e3f591f9476104b5
katy millard <katy.millard@gmail.com>
2022-01-26 11:35:01 -0600

    Added Wait n years

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ import input from 'input';
 
 async function main() {
         const drivingAge = 18;
-        const myAge = 41;
+        const myAge = 15;
         const hasLicense = await input.confirm('Do you have your license?');
+        const n = (18 - myAge);
 
     if (myAge >= drivingAge && hasLicense) {
         console.log('You can drive!');
     }
 
     else {
-        console.log('You cannot drive!');
+        if (myAge < drivingAge){
+            console.log('Wait ' + n + ' years!');
+        }
+        else{
+            console.log('Go get your license!')
+        }
     }
 }
 
